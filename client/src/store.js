3b8b905fe46584930735fee617c82d7cfdf703a1
ryanadams <rcoleman.adams@gmail.com>
2022-06-26 20:36:12 -0500

    removed unneccessary npm thunk

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,12 @@
-import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
-import thunkMiddleware from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './Reducers/user'
 import golfReducer from './Reducers/golf';
-
-const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     golf: golfReducer
   }
-}, composedEnhancer);
+});
 
 export default store;
